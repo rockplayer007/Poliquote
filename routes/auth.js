c05d849c695b6c8cfb424fddd471aa986c510d41
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-06-08 17:26:03 +0200

    Added function to register users

--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,18 @@ var express = require('express');
 var router = express.Router();
 var conn = require("./database");
 
+router.post("/signup", function (req, res) {
+    var user = req.body;
+    conn.query("SELECT username FROM users WHERE username = ?", user.username, function (err, rows, fields) {
+        if (err) res.status(500).send("error in database");
+        else
+            if (rows.length == 0)
+                conn.query("INSERT INTO users SET ?", user, function (err, result) {
+                    if (err) res.status(500).send("error in database");
+                    else res.send(200).send("ok");
+                });
+            else res.status(400).send("username not available");
+    });
+});
+
 module.exports = router;
