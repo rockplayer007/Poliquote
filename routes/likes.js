3656df799b603a521660928dd7a6547111784eeb
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-06-08 20:48:55 +0200

    Added function to get the count of likes

--- routes/likes.js
+++ routes/likes.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var conn = require("./database");
- 
+
+router.get("/getlikescount", function (req, res) {
+    var targetId = req.body;
+    conn.query("SELECT id FROM likes WHERE target = ?", targetId, function (err, rows, fields) {
+        if (err) res.status(500).send("error in database");
+        else res.status(200).send(rows.lenght);
+    });
+});
+
 module.exports = router;
