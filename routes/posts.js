d4b330466e13cf0379e549fc9e0b293d408eb6d6
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2017-06-10 00:26:53 +0200

    Fixed function to get posts

--- routes/posts.js
+++ routes/posts.js
@@ -6,19 +6,26 @@ router.get("/getposts", function (req, res) {
     var query = "SELECT * FROM posts WHERE";
     try {
         var options = req.options;
-        if (options.search)
-            for (var key in options.search)
-                if (key)
-                    query += util.format(" %s = %s AND", key, options.search[key]);
-        if (options.startDate && options.endDate) query += util.format(" datetime >= %s AND datetime <= %s", options.startDate, options.endDate);
-        query += ";"; //Add closing semicolon
-        //Check if there is an "AND" at the end of the query and remove it
-        query = query.replace("AND;", ';').replace("WHERE;", ';');
-        //Set a query limit (if present)
-        if (options.limit) query = query.replace(';', util.format("LIMIT %s;", options.limit));
+        if (options) {
+            if (options.search)
+                for (var key in options.search)
+                    if (key)
+                        query += util.format(" %s = %s AND", key, options.search[key]);
+            if (options.startDate && options.endDate) query += util.format(" datetime >= %s AND datetime <= %s", options.startDate, options.endDate);
+            query += ";"; //Add closing semicolon
+            //Check if there is an "AND" at the end of the query and remove it
+            query = query.replace("AND;", ';').replace("WHERE;", ';');
+            //Set a query limit (if present)
+            if (options.limit) query = query.replace(';', util.format("LIMIT %s;", options.limit));
+        }    
+        else {
+            query = query.replace("WHERE", ';');
+        }
     } catch (ex) {
         res.status(400).send("bad query options");
+        return;
     }
+    
     conn.query(query, function (err, rows, fields) {
         if (err) res.status(500).send("error in database");
         else res.status(200).send(rows);    
